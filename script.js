a429766f1447aa02d93f072d61b8c010c6b630e6
Your Name <julianverquinal2006@example.com>
2025-09-27 20:04:15 +0800

    all changes added

--- script.js
+++ script.js
@@ -1,33 +1,47 @@
-const fetchDefinition = async () =>{
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 
-    try {
-        const wordInput = document.getElementById("word").value.toLowerCase();
-        const fetchWord = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${wordInput}`);
+const button = document.getElementById("searchButton");
+const sound = document.getElementById("sound");
 
-        if(!fetchWord.ok){
-            throw new Error(`There's no such word as ${wordInput}`);
+const fetchWord = async() =>{
+    try{
+
+        let input  =document.getElementById("word").value.toLowerCase();
+         let result = document.querySelector(".result");
+
+        const response = await fetch(`${url}${input}`);
+        if (!response.ok) {
+            throw new Error(`There is no such Word as "${input}"`);
         }
 
-        const data = await fetchWord.json();
-        
-        const wordDefinition = data[0].meanings[0].definitions[0].definition;
-        
+        const data = await response.json();
+        console.log(data);
 
-        
-        
-            const wordDef = document.getElementById("wordDefinition");
-           
+        result.innerHTML =`<div class="result">
+                    <div id="word">
+                        <h3>${input}</h3>
+                        <button>
+                            <i class="fa fa-volume-up"></i>
+                        </button>
+                    </div>
 
-            wordDef.textContent = wordDefinition;
-                
-        
+                    <div class="details">
+                        <p>${data[0].meanings[0].partOfSpeech}</p>
+                        <p> /${data[0].phonetics[1].text}/</p>
+                    </div>
+                    <div>
+                    <p class="wordMeaning">
+                       ${data[0].meanings[0].definitions[0].definition}
+                    <p class="wordExample">
+                        ${data[0].meanings[0].definitions[0].example || "no example available"}    
+                    </p>
+                    </div>
+                </div>`;
 
-    } catch (error) {
-        console.log(error);
+
+    }catch(error){
+        console.error(error);
     }
-
 }
 
-const button = document.getElementById("buttonW");
-
-button.addEventListener("click", fetchDefinition);
+button.addEventListener("click", fetchWord);
